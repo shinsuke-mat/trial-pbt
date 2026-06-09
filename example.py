ecc625973f75fb6b167f687ca24e9937a01b5c61
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2026-06-09 15:18:32 +0900

    more

--- example.py
+++ example.py
@@ -23,4 +23,47 @@ def is_magic_condition(lst):
 def fizzbuzz_dummy(n):
     if n % 12341234 * 2 == 0:
         return 'aa'
+    if str(n) == 'abcdEFG':
+        return 'bb'
     return n
+
+def is_semver(v):
+    import re
+    numeric = '(0|[1-9][0-9]*)'
+    pattern = r'^%s\.%s\.%s$' % (numeric, numeric, numeric)
+    return re.match(pattern, v) != None
+
+
+from enum import Enum
+class Type(Enum):
+    EQUALITY = 1
+    ISOSCELE = 2
+    SCALENE = 3
+    INVALID = 4
+
+def classify(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return Type.INVALID
+
+    if a == b and b == c:
+        return Type.EQUALITY
+
+    if a == b:
+        if a + b > c:
+            return Type.ISOSCELE
+        return Type.INVALID
+
+    if a == c:
+        if a + c > b:
+            return Type.ISOSCELE
+        return Type.INVALID
+
+    if b == c:
+        if b + c > a:
+            return Type.ISOSCELE
+        return Type.INVALID
+
+    if a < b + c and b < a + c and c < a + b:
+        return Type.SCALENE
+
+    return Type.INVALID
--- test_example.py
+++ test_example.py
@@ -1,4 +1,4 @@
-from hypothesis import given, strategies as st, settings
+from hypothesis import given, strategies as st, settings, Verbosity
 from example import *
 
 """
@@ -157,3 +157,143 @@ def test_generator8(lst):
 
     # Shrinkingと呼ぶらしい．エンジンはConjecture Engine
     # https://hypothesis.works/articles/how-hypothesis-works/
+
+"""
+sematic-versioningに対するPBTを試してみる
+やはり生成機がない，かつ別実装を用意できない場合はPBTがやりにくい
+部分仕様を検証するのが良さそう
+"""
+@given(st.text().filter(lambda s: "." not in s))
+def test_semver1(ver):
+    assert not is_semver(ver)
+    # でもこれはあまりにも限定的．
+    # 以下の指摘がしっくりくる．
+    # > PBTはしばしば「正しい入力を探す技術」ではなく、
+    # > 「受理された入力の構造的不変条件を検証する技術」として使われます
+
+
+@given(st.integers(min_value=0), st.integers(min_value=0), st.integers(min_value=0))
+def test_semver2(major, minor ,fix):
+    valid = f"{major}.{minor}.{fix}"
+    print(valid)
+    assert is_semver(valid)
+    # このやり方は合理的．
+    # プロダクト側とテスト側の手段が違うから．
+    # プロダクトが正規表現，テスト側が数字の合成なので検証として成立する
+
+    # 生成機が簡単に作れる問題だ，ともいえるか．
+
+    # 上記が可能ならmutation propertyが使える
+    assert not is_semver("x" + valid)
+    assert not is_semver(valid + "x")
+    assert not is_semver(f"{major}{minor}.{fix}")
+    assert not is_semver(f"{-1}.{minor}.{fix}")
+    assert is_semver(f"{major+1}.{minor}.{fix}")
+
+    # > プロダクトが正規表現，テスト側が数字の合成なので検証として成立する
+    # これは何かおもしろい
+    # 核となるsemverの表現方法が2つある，ともみなせる
+
+"""
+バカみたいにassumeしまくってみる
+"""
+from hypothesis import assume, HealthCheck
+@given(st.integers())
+def test_assume(n):
+    assume(n == 0)
+    assert True
+    # healthcheck警告が出る．50回乱数を試したが一つたりともassumeを通らなかった．
+    # hypoがよく考えて作られている
+
+    # memo healthcheckの無効:
+    # @settings(suppress_health_check=[HealthCheck.filter_too_much])
+
+
+from hypothesis import target
+@given(st.lists(st.integers(max_value=100, min_value=0)))
+def test_generator9(lst):
+    target(- len(lst))
+    print(lst)
+
+"""
+Myersの三角形でPBTを試す
+"""
+from example import Type
+@given(st.integers(min_value=1), st.integers(min_value=1), st.integers(min_value=1))
+def test_classify_permutation(a, b, c):
+    type = classify(a, b, c)
+    assert classify(b, a, c) == type
+    assert classify(c, b, a) == type
+    # 3辺の順序を入れ替えても結果は同じ
+
+@given(st.integers(min_value=1), st.integers(min_value=1), st.integers(min_value=1), st.integers(min_value=1))
+def test_classify_multiply(a, b, c, k):
+    assert classify(a, b, c) == classify(k * a, k * b, k * c)
+    # k倍しても結果は同じ
+
+@given(st.integers(min_value=1))
+def test_classify_equality(n):
+    assert classify(n, n, n) == Type.EQUALITY
+    assert classify(n + 1, n + 1, n + 1) == Type.EQUALITY
+
+@given(n=st.integers(min_value=2), offset=st.integers(min_value=1))
+def test_classify_isoscele(n, offset):
+    m = n + offset
+    if m >= 2 * n:
+        m = 2 * n - 1
+    assert classify(n, n, m) == Type.ISOSCELE
+
+@given(st.integers(min_value=1), st.integers(min_value=1))
+def test_classify_invalid1(a, b):
+    c = a + b
+    assert classify(a, b, c) == Type.INVALID
+
+@given(st.integers(min_value=1, max_value=100),
+       st.integers(min_value=1, max_value=100),
+       st.integers(min_value=1, max_value=100))
+def test_classify_scalene1(a, b, c):
+    assume(a != b and b != c and c != a and       # まず全辺の長さが違いつつ
+           a < b + c and b < a + c and c < a + b) # 三角形として成立する場合
+    assert classify(a, b, c) == Type.SCALENE
+    # これはくるしい．不等辺三角形は全部値が異なるという消極的な構造を持つから
+    # あとassume()の多用はよくないらしい．生成してrejectを繰り返すので無駄が多い．
+
+from hypothesis import event
+@given(st.integers(min_value=1, max_value=100),
+       st.integers(min_value=1, max_value=100),
+       st.integers(min_value=1, max_value=100))
+def test_classify_scalene2(a, b, c):
+    assume(a != b and b != c and c != a)
+    if a < b + c and b < a + c and c < a + b:
+        event("SCALENE") # 分岐を使っているのでeventでサンプル数を計測する
+        assert classify(a, b, c) == Type.SCALENE
+    else:
+        event("INVALID")
+        assert classify(a, b, c) == Type.INVALID
+    # 上のテストの亜種．ifで不等辺と不成立を切り分けている
+    # ただ2つのプロパティをまとめるのはよくないらしい．片方の検証が不十分な可能性があるから
+    # 今回のような複雑な入力の生成規則は別途ジェネレータを作るべきか
+
+"""
+targeted PBTを試す
+"""
+@given(st.floats(0, 1e100), st.floats(0, 1e100), st.floats(0, 1e100))
+def test_associativity_with_target(a, b, c):
+    ab_c = (a + b) + c
+    a_bc = a + (b + c)
+    difference = abs(ab_c - a_bc)
+    print(difference)
+    target(difference)  # Without this, the test almost always passes
+    assert difference < 2.0
+
+@given(st.integers(0, 1e10))
+def test_target(a):
+    print(a)
+    target(a)
+    assert a != 1e10 - 10
+
+@given(st.integers(0, 1e10))
+#@settings(verbosity=Verbosity.verbose, database=None)
+def test_target2(a):
+    print(a)
+    assert a <= 1e10 - 10
